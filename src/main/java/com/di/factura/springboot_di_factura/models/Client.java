852320b28be9f283620e1478b52616de003e7192
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-09-02 13:18:15 -0600

    models

--- src/main/java/com/di/factura/springboot_di_factura/models/Client.java
+++ src/main/java/com/di/factura/springboot_di_factura/models/Client.java
@@ -1,4 +1,25 @@
 package com.di.factura.springboot_di_factura.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Client {
+    private String name;
+    private String lastName;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
--- src/main/java/com/di/factura/springboot_di_factura/models/Invoice.java
+++ src/main/java/com/di/factura/springboot_di_factura/models/Invoice.java
@@ -1,4 +1,37 @@
 package com.di.factura.springboot_di_factura.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class Invoice {
+    @Autowired
+    private Client client;
+    private String description;
+    private List<Item> items;
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
